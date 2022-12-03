adfdbb64469fd24fe8b05b0e5d65c2b3d2943c35
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-12-03 23:46:54 +0300

    Created a new program into folder Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+void DefineRowOfCubes(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+       double result =  Math.Pow(count,3);
+       Console.Write ($"{result}, ");
+       count += 1;
+    }
+}
+
+Console.WriteLine($"Введите число, для определения кубов чисел от 1 до данного числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+DefineRowOfCubes(number);
