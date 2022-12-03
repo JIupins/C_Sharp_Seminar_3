a8e82a6ed0bf05e94ede7baf9930bb4b9d692bf4
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-12-03 22:01:14 +0300

    Created a new program into folder Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int FlipNumber(int num)
+{
+    int newNum = default;
+    while (num > 0)
+    {
+        int bufer = num % 10;
+        newNum = newNum * 10 + bufer; // Почему не работает составной оператор присваивания "newNum *= 10 + bufer;"? Подозреваю что он думает что "newNum = newNum * (10 + bufer);"
+        num /=10;
+    }
+    return newNum;
+}
+
+void CompareNumbers(int oneNum, int twoNum)
+{
+    if (oneNum == twoNum) Console.WriteLine ($"Введеное число {oneNum} является полиндромом!");
+    else Console.WriteLine ($"Введеное число {oneNum} не является полиндромом!");
+}
+
+Console.Write("Введите пятизначное число: ");
+int mainNumber = Convert.ToInt32(Console.ReadLine());
+
+int reverseNumber = FlipNumber (mainNumber);
+CompareNumbers (mainNumber, reverseNumber);
